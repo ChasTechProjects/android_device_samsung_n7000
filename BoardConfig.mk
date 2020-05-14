8ddcc07e1875694c20aa39084c056663c50bb8a4
leewp14 <lee.wp14@gmail.com>
2020-05-14 13:38:45 +0800

    n7000: remove TWRP build support
    Not needed currently. Additionally, the inclusion of twrp.fstab in /etc conflicts with Paranoid Android quartz build.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,20 +31,5 @@ TARGET_KERNEL_CONFIG := lineageos_n7000_defconfig
 # assert
 TARGET_OTA_ASSERT_DEVICE := galaxynote,n7000,N7000,GT-N7000
 
-# TWRP
-TW_THEME := portrait_hdpi
-TWRP_NEW_THEME := true
-HAVE_SELINUX := true
-TW_MAX_BRIGHTNESS := 255
-TW_INCLUDE_CRYPTO := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_NO_REBOOT_BOOTLOADER := true
-TW_INTERNAL_STORAGE_PATH := "/sdcard0"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard0"
-TW_EXTERNAL_STORAGE_PATH := "/sdcard1"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard1"
-TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
-TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/platform/s5p-tmu/temperature"
-
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/n7000/BoardConfigVendor.mk
