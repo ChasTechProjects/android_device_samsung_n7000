0cb4cc484009da3001ed64ee3ac25c46e6dc4093
leewp14 <lee.wp14@gmail.com>
2020-06-03 15:04:10 +0800

    n7000: properly set PRODUCT_AAPT_CONFIG
    The default config will follow what is defined in PRODUCT_AAPT_PREF_CONFIG, and PRODUCT_AAPT_CONFIG will act as a fallback value.
    In this case, if xhdpi is unavailable, at least we could fallback to hdpi first, then to normal.
    
    Refer: https://gitlab.com/aosplus/device_moto_shamu/commit/82a1bb146f56ae7d9c01bdb0f7fe7eef21f052ea
    
    Change-Id: I6320667aa896f44749498199049350f7836b1fb1

--- n7000.mk
+++ n7000.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc
 
 # This device is xhdpi.
-PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_LOCALES += xhdpi
 
