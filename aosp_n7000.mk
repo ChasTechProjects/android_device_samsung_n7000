ef23e07849323cb610fed862d1bfd35caa27cc60
ChasTechProjects <44618658+ChasTechProjects@users.noreply.github.com>
2020-11-17 05:45:36 +0000

    Adapt for AOSP

--- aosp_n7000.mk
+++ aosp_n7000.mk
@@ -18,7 +18,7 @@
 PRODUCT_RELEASE_NAME := GT-N7000
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration.
@@ -30,7 +30,7 @@ TARGET_SCREEN_WIDTH := 800
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7000
-PRODUCT_NAME := lineage_n7000
+PRODUCT_NAME := aosp_n7000
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-N7000
